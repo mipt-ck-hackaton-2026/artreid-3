585c821f3b26fbe591b67718cae58462c34f3b86
AndreyQuantum <zelentsov1254@gmail.com>
2026-04-04 01:46:17 +0200

    init structure

---
+++ src/test/java/com/ck/hackaton/artreid_3/artreid3/Artreid3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ck.hackaton.artreid_3.artreid3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Artreid3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
