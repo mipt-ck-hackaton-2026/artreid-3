8f299588d998fad00398ce5349e798b1a898e9b7
AndreyQuantum <zelentsov1254@gmail.com>
2026-04-19 13:38:42 +0200

    refactor: replace map-based health response with dedicated HealthResponseDTO

---
+++ src/main/java/com/ck/hackaton/artreid_3/artreid3/dto/HealthResponseDTO.java
@@ -0,0 +1,13 @@
+package com.ck.hackaton.artreid_3.artreid3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HealthResponseDTO {
+    private String status;
+    private String version;
+}
